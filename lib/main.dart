b149152cbf8d4dae42c3bf15d129f0117f91886e
hermes curo <hermescurov5@gmail.com>
2025-11-08 23:30:09 -0500

    añadiendo un auth context, models y un servicio

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,11 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await dotenv.load(fileName: ".env");
+
   final auth = AuthProvider();
   await auth.tryAutoLogin();
 
-  await dotenv.load(fileName: ".env");
   runApp(
     MultiProvider(
       providers: [
